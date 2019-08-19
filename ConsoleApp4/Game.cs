7792f8423701792c54a7576958cc672243c845d7
Ryan LaCombe <s198023@students.aie.edu>
2019-08-19 17:37:28 -0500

    Add project files.

---
+++ ConsoleApp4/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Game
+    {
+        int numberOne = 10;
+        int numberTwo = 200;
+        public void Start()
+        {
+            int biggestNumber = ReturnBiggestNumber(numberOne, numberTwo);
+
+            Console.WriteLine("Of " + numberOne + " and " + numberTwo + " the biggest number is " + biggestNumber);
+            Console.ReadKey();
+        }
+
+        int ReturnBiggestNumber(int first, int second)
+        {
+            if(first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
